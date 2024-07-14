88ca7ebcb9e7b0543a482ee5ed8a3abe8f18b6e9
meursw <filippos-str@hotmail.gr>
2024-07-14 22:48:10 +0300

    Added some documentation

--- DP_max_array_sum.c
+++ DP_max_array_sum.c
@@ -2,6 +2,22 @@
 #include <time.h>
 #include <stdlib.h>
 
+/*
+Basic dynamic programming problem.
+    Given an array of elements (integers) find the maximum consecutive sum inside the array.
+
+How to solve the problem:
+    1) Define the problems: We split the problems into smaller arrays from element 1-j (where j = {1,...,n})
+    Basically we approach the problem from a bottom up approach, solving smaller problems first (sub-arrays)
+    and from these solutions we can construct the final solution. (An integer, the maximum sum.)
+    
+    2) Define the choices: In each iteration we may choose to include the solution of the previous problem, continuing the 
+    list of numbers from the last index, or starting anew, from index j.
+
+    3) Define the recursive relation: This is how we choose from the choices mentioned above. We take the max between the 
+    solution of the previous problem and the value of the element at index j.
+*/
+
 int* make_array(unsigned int);
 int dp_max_sum(int*, unsigned int);
 
